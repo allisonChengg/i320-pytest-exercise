b95a1171ec13420fdaf375634fe61b2a232f59e8
Allison Cheng <alli.cheng80@gmail.com>
2026-03-15 23:43:55 -0400

    add tests for 10 character strings with non-digit chars

--- test_my_code.py
+++ test_my_code.py
@@ -11,3 +11,9 @@ def test_invalid_formats():
     fix_phone_num("555-442-98761")
   with pytest.raises(ValueError):
     fix_phone_num("(3213) 654 3333")
+
+def test_non_digit_characters():
+  with pytest.raises(ValueError):
+    fix_phone_num("334dfdee45")
+  with pytest.rises(ValueError):
+    fix_phone_num("abcdefghij")
