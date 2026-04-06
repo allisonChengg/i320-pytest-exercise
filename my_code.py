e8a8eaeb8c25df4f3ea5256f5d538d1a92908fe4
Allison Cheng <alli.cheng80@gmail.com>
2026-04-05 21:13:40 -0500

    created workflow

--- my_code.py
+++ my_code.py
@@ -1,7 +1,12 @@
 import pytest
 
 def fix_phone_num(phone_num_to_fix):
+  
   if len(phone_num_to_fix) != 10:
+    raise ValueError("Can only format numbers that are exactly 10 digits long")
+  
+  # add an additional test that fails with a specific message if the phone number is longer than 10 digits but starts with '+1'
+  if len(phone_num_to_fix) >= 10 and phone_num_to_fix[0:2] = '+1':
     raise ValueError("Can only format numbers that are exactly 10 digits long")
       
   if not phone_num_to_fix.isdigit():
@@ -11,4 +16,5 @@ def fix_phone_num(phone_num_to_fix):
   three_part = phone_num_to_fix[3:6]
   four_part = phone_num_to_fix[6:]
       
-  return f"({area_code}) {three_part} {four_part}"
+  fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part 
+  return fixed_num
--- test_my_code.py
+++ test_my_code.py
@@ -1,7 +1,8 @@
+import pytest
 from my_code import fix_phone_num
 
 def test_fix_phone_num():
-  assert fix_phone_num("5125558823") == '(512) 555 8823"
+  assert fix_phone_num("5125558823") == '(512) 555 8823'
   assert fix_phone_num("5554429876") == '(555) 442 9876'
   assert fix_phone_num("3216543333") == '(321) 654 3333'
 
@@ -15,5 +16,9 @@ def test_invalid_formats():
 def test_non_digit_characters():
   with pytest.raises(ValueError):
     fix_phone_num("334dfdee45")
-  with pytest.rises(ValueError):
+  with pytest.raises(ValueError):
     fix_phone_num("abcdefghij")
+
+def test_fix_phone_num():
+  with pytest.raises(ValueError):
+    fix_phone_num(+1 512 555 8823")
